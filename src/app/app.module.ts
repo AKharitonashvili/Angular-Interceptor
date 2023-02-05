13f5b86baa4e8313f848a25c590dd57b1e0767da
sandro sandro <sandroxari@gmail.com>
2023-02-05 16:01:03 +0400

    feature: set routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,9 +10,6 @@ import {
   TokenInterceptor,
 } from './interceptors';
 import { LanguageInterceptor } from './interceptors/accept-language.interceptor';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DashboardModule } from './dashboard/dashboard.module';
 
 const modules = [DashboardModule];
@@ -46,16 +43,7 @@ const interceptors = [
 ];
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    NgbModule,
-    BrowserAnimationsModule,
-    ReactiveFormsModule,
-    FormsModule,
-    ...modules,
-  ],
+  imports: [BrowserModule, AppRoutingModule, ...modules],
   declarations: [AppComponent],
   providers: [...interceptors],
   bootstrap: [AppComponent],
