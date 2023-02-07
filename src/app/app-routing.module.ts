ba48ea22dcfda8034afc3335c70a5e14d164eb28
sandro sandro <sandroxari@gmail.com>
2023-02-07 21:48:34 +0400

    feature: remove info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: '/Home', pathMatch: 'full' },
   {
-    path: 'Info',
-    loadChildren: () => import('./info/info.module').then((m) => m.InfoModule),
-  },
-  {
     path: 'Posts',
     loadChildren: () =>
       import('./user-posts/user-posts.module').then((m) => m.UserPostsModule),
